d82e16e5f60b37c7dab0cee6ecefaa9b6e50f691
Romantic <595159090004@dpu.ac.th>
2018-02-09 23:32:59 +0700

    hello react-middlewares

---
+++ webpack.config.js
@@ -0,0 +1,77 @@
+const path = require('path')
+const webpack = require('webpack')
+
+const context = path.resolve(__dirname, 'src')
+const cssModuleRules = [
+  'style-loader',
+  {
+    loader: 'css-loader',
+    options: {
+      module: true,
+      sourceMap: true,
+      localIdentName: '[path][name]__[local]--[hash:base64:5]'
+    }
+  },
+  {
+    loader: 'postcss-loader',
+    options: {
+      sourceMap: true
+    }
+  }
+]
+
+module.exports = {
+  devtool: 'eval-source-map',
+  context,
+  entry: {
+    app: [
+      'react-hot-loader/patch',
+      'normalize.css',
+      './index'
+    ]
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name].js'
+  },
+  resolve: {
+    alias: {
+      Lib: path.resolve(context, 'lib'),
+      Actions: path.resolve(context, 'actions'),
+      Middlewares: path.resolve(context, 'middlewares'),
+      Containers: path.resolve(context, 'containers'),
+      Components: path.resolve(context, 'components')
+    },
+    extensions: [".js", ".json", ".scss"]
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, enforce: 'pre', loader: 'eslint-loader' },
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
+      { test: /\.css$/, use: cssModuleRules },
+      {
+        test: /\.scss$/,
+        use: [
+          ...cssModuleRules,
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    hot: true,
+    historyApiFallback: true,
+    proxy: {
+      "/api": "http://localhost:3000"
+    }
+  }
+}
