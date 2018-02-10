bd7e049e5db1104811ea06deeac0bb4b727d56e7
Romantic <595159090004@dpu.ac.th>
2018-02-10 19:26:57 +0700

    Middlewares: Unique ID

--- src/actions/article.js
+++ src/actions/article.js
@@ -4,13 +4,12 @@ import {
   DELETE_ARTICLE 
 } from 'Actions'
 
-let nextId = 1
 
 export function createArticle(value) {
   return {
     type: CREATE_ARTICLE,
     article: {
-      id: nextId++,
+      id: +new Date(),
       value
     }
   }
